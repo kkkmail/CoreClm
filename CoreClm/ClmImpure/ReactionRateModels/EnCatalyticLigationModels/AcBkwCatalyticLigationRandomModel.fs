391bbf7e81e208a793a989a16423bb2d1557974c
kkkmail <kkk@jfcglobal.net>
2020-12-24 08:11:41 -0500

    FSV:5.0, V:6.0.1.0 - Activated Catalysts

---
+++ CoreClm/ClmImpure/ReactionRateModels/EnCatalyticLigationModels/AcBkwCatalyticLigationRandomModel.fs
@@ -0,0 +1,36 @@
+﻿namespace ClmImpure.ReactionRateModels
+
+open Clm.Substances
+open Clm.ReactionTypes
+open Clm.ReactionRatesBase
+open Clm.ReactionRateParams
+open ClmImpure.ReactionRateFunctions
+open ClmImpure.ReactionRateModels.ReactionRateModelBase
+open ClmImpure.ReactionRateModels.LigationModel
+
+module AcBkwCatalyticLigationRandomModel =
+
+    type AcBkwCatalyticLigationRandomParamWithModel =
+        {
+            acBkwCatLigationParam : AcBkwCatalyticLigationRandomParam
+            ligationModel : LigationModel
+        }
+
+
+    type AcBkwCatalyticLigationRandomModel (p : AcBkwCatalyticLigationRandomParamWithModel) =
+        inherit RateModel<AcBkwCatalyticLigationRandomParamWithModel, AcBkwCatalyticLigationReaction>(p)
+
+        let calculateCatSynthRates rnd t (AcBkwCatalyticLigationReaction (s, c)) =
+            {
+                reaction = s
+                acCatalyst = c
+                getCatEnantiomer = getEnantiomer
+                acCatReactionCreator = AcBkwCatalyticLigationReaction
+                getBaseRates = p.ligationModel.getRates rnd
+                eeParams = p.acBkwCatLigationParam.acBkwCatLigRndEeParams
+                rateGenerationType = t
+                rnd = rnd
+            }
+            |> calculateAcCatRates
+
+        member model.getRates rnd t r = getRatesImpl model.rateDictionary getEnantiomer (calculateCatSynthRates rnd t) r
---
+++ CoreClm/ClmImpure/ReactionRateModels/EnCatalyticLigationModels/AcBkwCatalyticLigationSimilarModel.fs
@@ -0,0 +1,44 @@
+﻿namespace ClmImpure.ReactionRateModels
+
+open Clm.Substances
+open Clm.ReactionTypes
+open Clm.ReactionRatesBase
+open Clm.ReactionRateParams
+open ClmImpure.ReactionRateFunctions
+open ClmImpure.ReactionRateModels.AcBkwCatalyticLigationRandomModel
+
+module AcBkwCatalyticLigationSimilarModel =
+
+    type AcBkwCatalyticLigationSimilarParamWithModel =
+        {
+            acBkwCatLigModel : AcBkwCatalyticLigationRandomModel
+            peptideBondData : PeptideBondData
+            acBkwCatLigSimParam : AcCatRatesSimilarityParam
+        }
+
+
+    type AcBkwCatalyticLigationSimilarModel (p : AcBkwCatalyticLigationSimilarParamWithModel) =
+        let calculateSimRatesImpl rnd t (AcBkwCatalyticLigationReaction (s, c)) =
+            {
+                reaction = s
+                acCatalyst = c
+                getReactionData = fun r -> p.peptideBondData.findSameBondSymmetry r.peptideBond
+                getMatchingReactionMult = fun x -> x
+                inverse = fun r -> r.peptideBond
+                getCatEnantiomer = getEnantiomer
+                acCatReactionCreator = AcBkwCatalyticLigationReaction
+                getCatReactEnantiomer = getEnantiomer
+                simReactionCreator = fun e -> p.peptideBondData.findSameBond e
+                getBaseRates = p.acBkwCatLigModel.inputParams.ligationModel.getRates rnd
+                getBaseCatRates = p.acBkwCatLigModel.getRates rnd t
+                acSimParams = p.acBkwCatLigSimParam
+                eeParams = p.acBkwCatLigModel.inputParams.acBkwCatLigationParam.acBkwCatLigRndEeParams
+                rateDictionary = p.acBkwCatLigModel.rateDictionary
+                rateGenerationType = t
+                rnd = rnd
+            }
+            |> calculateAcSimRates
+
+        member _.getRates rnd t r = calculateSimRatesImpl rnd t r
+        member _.inputParams = p
+        member _.getAllRates() = getAllRatesImpl p.acBkwCatLigModel.rateDictionary
