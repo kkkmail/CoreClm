7e09b2a533cce72140f25445add0d0faf218465c
kkkmail <kkk@jfcglobal.net>
2021-03-28 11:22:50 -0400

    V:6.0.6

---
+++ Sql/ContGen/select/SELECT_RunQueue_EstComplTime_606.sql
@@ -0,0 +1,47 @@
+use clm606
+go
+
+declare @now datetime
+set @now = getdate()
+
+;with w as
+(
+	select
+		runQueueOrder
+		,case when callCount > 0 then progress / callCount else 0 end as averageStep
+		,d.clmDefaultValueId
+		,progress
+		,yRelative
+		,runQueueStatusId
+		,errorMessage
+		,case 
+			when runQueueStatusId = 3 then q.modifiedOn
+			when startedOn is not null and progress > 0 then dateadd(second, datediff(second, startedOn, @now) / progress, startedOn) 
+			else null 
+		 end as estCompl
+		,cast(
+			case
+				when runQueueStatusId = 3 then datediff(second, q.startedOn, q.modifiedOn) / (3600.0 * 24.0)
+				when startedOn is not null and progress > 0 then datediff(second, startedOn, @now) / progress / (3600.0 * 24.0) 
+				else null 
+			end as money) as totalRunTime
+		,runQueueId
+		,workerNodeName
+		,q.startedOn
+		,q.modifiedOn
+	from
+		ClmDefaultValue d 
+		inner join ClmTask t on d.clmDefaultValueId = t.clmDefaultValueId
+		inner join ModelData m on m.clmTaskId = t.clmTaskId
+		inner join RunQueue q on q.modelDataId = m.modelDataId
+		left outer join WorkerNode w on q.workerNodeId = w.workerNodeId
+	--where progress <> 1 and runQueueStatusId = 2
+)
+select * 
+from w
+where
+	estCompl is not null
+	--and clmDefaultValueId >= 4004000000
+	--and estCompl < dateadd(day, 1, @now)
+	--and totalRunTime > 0.5
+order by estCompl desc
