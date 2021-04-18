d24d7339b846844875dbac7d099edb810796a66d
kkkmail <kkk@jfcglobal.net>
2021-04-18 09:29:30 -0400

    V:6.0.7

--- CoreClm/ClmSys/VersionInfo.fs
+++ CoreClm/ClmSys/VersionInfo.fs
@@ -10,29 +10,29 @@ module VersionInfo =
     ///     1. Internal messaging structures change and messages can no longer be successfully transferred among components.
     ///     2. Some other updates were performed and we need to inform worker nodes that they need to upgrade.
     ///     3. Version number (below) is increased.
-    let messagingDataVersion = MessagingDataVersion 118
+    let messagingDataVersion = MessagingDataVersion 120
 
 
     /// ! Do not forget to update versionNumber in VersionInfo.ps1 when this parameter is updated !
     ///
     /// This is an overall system version.
     [<Literal>]
-    let VersionNumberValue = "6.0.6"
+    let VersionNumberValue = "6.0.7"
 
 
     /// A base name, which controls the database name and a working folder name.
     /// It is loosely the same as the version number.
     /// It must be updated when the old version is still running (for days) but the new version needs to be deployed.
     [<Literal>]
-    let ClmBaseName = "clm606"
+    let ClmBaseName = "clm607"
 
 
     [<Literal>]
-    let MsgSvcBaseName = "msg606"
+    let MsgSvcBaseName = "msg607"
 
 
     [<Literal>]
-    let WorkerNodeSvcBaseName = "wns606"
+    let WorkerNodeSvcBaseName = "wns607"
 
 
     /// Default port on which messaging communication is performed.
