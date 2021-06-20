00d834c217debc09758b472b40107405ddcf2b3c
kkkmail <kkk@jfcglobal.net>
2021-06-20 15:39:51 -0400

    V:6.0.7.4

---
+++ Commands/add__063.bat
@@ -0,0 +1,6 @@
+ContGenAdm.exe add -i 4005000068 -n 20 -m 3 -y 10 -t 250000 -r 100
+ContGenAdm.exe add -i 4005000069 -n 20 -m 3 -y 10 -t 250000 -r 100
+ContGenAdm.exe add -i 4005000070 -n 20 -m 3 -y 10 -t 250000 -r 100
+ContGenAdm.exe add -i 4005000071 -n 20 -m 3 -y 10 -t 250000 -r 100
+ContGenAdm.exe add -i 4005000072 -n 20 -m 3 -y 10 -t 250000 -r 100
+ContGenAdm.exe add -i 4005000073 -n 20 -m 3 -y 10 -t 250000 -r 100
---
+++ Sql/ContGen/functions/getAcFwdCatLigScarcity.sql
@@ -0,0 +1,108 @@
+--declare @clmDefaultValueId bigint
+--set @clmDefaultValueId = 4005000020
+
+
+drop function if exists dbo.getAcFwdCatLigScarcity
+go
+
+create function dbo.getAcFwdCatLigScarcity(@clmDefaultValueId bigint)
+returns float
+as
+begin
+	declare @json nvarchar(max), @retVal float
+	select @json = defaultRateparams from ClmDefaultValue where clmDefaultValueId = @clmDefaultValueId
+
+	;with t1 as
+	(
+		select 
+			 b.[key] as yKey
+			,c.*
+		from openjson(@json) a
+		cross apply openjson(a.[value]) as b
+		cross apply openjson(b.[value]) as c
+		where a.[key] = 'rateParams' and c.[key] = 'Fields'
+	)
+	,t2 as
+	(
+		select b.* 
+		from t1
+		cross apply openjson(t1.[value]) as a
+		cross apply openjson(a.[value]) as b
+		where b.[key] = 'Fields'
+	)
+	,t3 as
+	(
+		select b.* 
+		from t2
+		cross apply openjson(t2.[value]) as a
+		cross apply openjson(a.[value]) as b
+		where b.[key] = 'acFwdCatLigParam'
+	)
+	,t4 as
+	(
+		select a.* 
+		from t3
+		cross apply openjson(t3.[value]) as a
+		where a.[key] = 'acFwdCatLigRndEeParams'
+	)
+	,t4_1 as
+	(
+		select a.* 
+		from t4
+		cross apply openjson(t4.[value]) as a
+		where a.[key] = 'Fields'
+	)
+	,t5 as
+	(
+		select b.*
+		from t4_1
+		cross apply openjson(t4_1.[value]) as a
+		cross apply openjson(a.[value]) as b
+		where b.[key] = 'rateMultiplierDistr'
+	)
+	,t6 as
+	(
+		select a.* 
+		from t5
+		cross apply openjson(t5.[value]) as a
+		where a.[key] = 'Fields'
+	)
+	,t7 as
+	(
+		select b.*
+		from t6
+		cross apply openjson(t6.[value]) as a
+		cross apply openjson(a.[value]) as b
+		where b.[key] = 'Fields'
+	)
+	,t8 as
+	(
+		select b.* 
+		from t7
+		cross apply openjson(t7.[value]) as a
+		cross apply openjson(a.[value]) as b
+		where b.[key] = 'distributionParams'
+	)
+	,t9 as
+	(
+		select a.* 
+		from t8
+		cross apply openjson(t8.[value]) as a
+		where a.[key] = 'threshold'
+	)
+	,t10 as
+	(
+		select a.* 
+		from t9
+		cross apply openjson(t9.[value]) as a
+		where a.[key] = 'Fields'
+	)
+	select @retval = cast(a.[value] as float) * 1.0E09
+	from t10
+	cross apply openjson(t10.[value]) as a
+
+	--select @retVal
+
+	return @retval
+end
+go
