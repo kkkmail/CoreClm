0d39eec4088e164aab822347ed3d471cd3200289
kkkmail <kkk@jfcglobal.net>
2021-06-27 06:14:25 -0400

    V:6.0.8

--- Sql/ContGen/select/SELECT_RunQueue_ByMaxLastEE_607.sql
+++ Sql/ContGen/select/SELECT_RunQueue_ByMaxLastEE_607.sql
@@ -25,10 +25,10 @@ FROM RunQueue q
 inner join dbo.ModelData m on q.modelDataId = m.modelDataId
 inner join dbo.ClmTask t on m.clmTaskId = t.clmTaskId
 inner join dbo.WorkerNode w on q.workerNodeId = w.workerNodeId
-where runQueueStatusId <> 3 
+where runQueueStatusId <> 3
 	--and t.clmDefaultValueId = 4005000026
 order by [maxLastEe] desc
 
--- update RunQueue set runQueueStatusId = 0, errorMessage = null, workerNodeId = null, startedOn = null, modifiedOn = getdate() where runQueueId = 'C60AD91C-A9CC-47A3-A69C-C796C0A6D063'
+-- update RunQueue set runQueueStatusId = 0, errorMessage = null, workerNodeId = null, startedOn = null, modifiedOn = getdate() where runQueueStatusId = 4 and runQueueId in('8E41105D-EEB8-42A8-BB2A-B6F318236B88', 'D8CE8318-12F2-4CC7-9920-9CF22BA34D33')
 
 
