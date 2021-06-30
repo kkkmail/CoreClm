d825b13598617100c120da8fe6aee1e3488c9570
kkkmail <kkk@jfcglobal.net>
2021-06-30 16:40:56 -0400

    V:6.0.7.7

---
+++ Commands/add__065.bat
@@ -0,0 +1,3 @@
+ContGenAdm.exe add -i 4005000082 -n 20 -m 3 -y 10 -t 250000 -r 100
+ContGenAdm.exe add -i 4005000083 -n 20 -m 3 -y 10 -t 250000 -r 100
+ContGenAdm.exe add -i 4005000084 -n 20 -m 3 -y 10 -t 250000 -r 100
