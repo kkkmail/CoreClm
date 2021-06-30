70beb256386b3e0b357693ce5487f2467c4ad372
kkkmail <kkk@jfcglobal.net>
2021-06-30 16:33:16 -0400

    V:6.0.8

--- Sql/ContGen/select/SELECT_RunQueue_ByMaxLastEE_607.sql
+++ Sql/ContGen/select/SELECT_RunQueue_ByMaxLastEE_607.sql
@@ -29,6 +29,7 @@ where runQueueStatusId <> 3
 	--and t.clmDefaultValueId = 4005000026
 order by [maxLastEe] desc
 
--- update RunQueue set runQueueStatusId = 0, errorMessage = null, workerNodeId = null, startedOn = null, modifiedOn = getdate() where runQueueStatusId = 4 and runQueueId in('8E41105D-EEB8-42A8-BB2A-B6F318236B88', 'D8CE8318-12F2-4CC7-9920-9CF22BA34D33')
+-- update RunQueue set runQueueStatusId = 0, errorMessage = null, workerNodeId = null, startedOn = null, modifiedOn = getdate() where runQueueStatusId = 4 and runQueueId in ('8E41105D-EEB8-42A8-BB2A-B6F318236B88', 'D8CE8318-12F2-4CC7-9920-9CF22BA34D33')
+-- update RunQueue set runQueueStatusId = 0, errorMessage = null, workerNodeId = null, startedOn = null, modifiedOn = getdate() where runQueueStatusId = 4 and runQueueId in ('4EDA8CDE-F4C9-4785-BA5A-832BDF36A777')
 
 
