4459ccfb9057e93d8905541fad5dbd1fba1a1898
kkkmail <kkk@jfcglobal.net>
2021-07-14 08:30:21 -0400

    V:6.0.8

--- Sql/ContGen/select/DisableClmTasks__607.sql
+++ Sql/ContGen/select/DisableClmTasks__607.sql
@@ -39,8 +39,10 @@ order by [clmTaskOrder]
 -- update [clm607].[dbo].[ClmTask] set [clmTaskStatusId] = 0 where [clmDefaultValueId] in (4005000022)
 
 
--- Temporarily stop 4005000085 - 4005000095 to make 4005000096 run first
+-- Temporarily stop 4005000085 - 4005000095 to make 4005000096 run first.
 -- update [clm607].[dbo].[ClmTask] set [clmTaskStatusId] = 1 where [clmDefaultValueId] in (4005000085, 4005000086, 4005000087, 4005000088, 4005000089, 4005000090, 4005000091, 4005000092, 4005000093, 4005000094, 4005000095)
 -- update [clm607].[dbo].[ClmTask] set [clmTaskStatusId] = 0 where [clmDefaultValueId] in (4005000085, 4005000086, 4005000087, 4005000088, 4005000089, 4005000090, 4005000091, 4005000092, 4005000093, 4005000094, 4005000095)
 
-
+-- Stop 4005000097 because it has all 0 and there are more important models to run.
+-- update [clm607].[dbo].[ClmTask] set [clmTaskStatusId] = 1 where [clmDefaultValueId] in (4005000097)
+-- update [clm607].[dbo].[ClmTask] set [clmTaskStatusId] = 0 where [clmDefaultValueId] in (4005000097)
