94cd60396dfeb67450c023b12181dbbc34b14269
kkkmail <kkk@jfcglobal.net>
2021-10-03 12:47:06 -0400

    NET6

---
+++ SqlProviderTest/Library.fs
@@ -0,0 +1,18 @@
+﻿namespace SqlProviderTest
+
+open FSharp.Data.Sql
+
+module Say =
+
+    [<Literal>]
+    let ConnectionString = "Data Source=localhost;Initial Catalog=clm608;Integrated Security=SSPI"
+
+
+    type sql = SqlDataProvider<Common.DatabaseProviderTypes.MSSQLSERVER, ConnectionString>
+
+
+    let load() =
+        let ctx = sql.GetDataContext()
+
+
+        0
