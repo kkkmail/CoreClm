2c23426ac2dbf33e3be977fe43ceac70caa64a00
kkkmail <kkk@jfcglobal.net>
2022-03-20 14:57:55 -0400

    V:7.0.0.0

--- CoreClm/Gillespie/SsaSolver.fs
+++ CoreClm/Gillespie/SsaSolver.fs
@@ -1,11 +1,65 @@
 ﻿namespace Gillespie
 
-open Clm.Distributions
-open Clm.ReactionTypes
-open Clm.Substances
+//open Clm.Distributions
+//open Clm.ReactionTypes
+//open Clm.Substances
 
 module SsaSolver =
     
+    type ChiralMolecule =
+        {
+            left: int
+            right : int
+        }
+        
+        member m.ee = (double (m.left - m.right)) / (double (max 1 (m.left + m.right)))
+        member m.n = m.left + m.right
+        
+        
+    /// TODO kk:20220320 - Use a custom type until the decision is made whether or not to use type Substance.
+    /// If Substance is to be used the replace this by a type alias.
+    type Species =
+        | Any of string
+        | Chiral of ChiralMolecule
+        
+//        member m.value = let (Molecule v) = m in v             
+        
+    
+    /// TODO kk:20220320 - Use a local type until the decision is made whether or not to use type Substance.
+    type ReactionRate =
+        | ReactionRate of double
+        
+        member r.value = let (ReactionRate v) = r in v
+        
+        
+    /// TODO kk:20220320 - Use a local type until the decision is made whether or not to use type Substance.
+    type ReactionNormalizedInfo =
+        {
+            inputNormalized : list<Species>
+            outputNormalized : list<Species>
+        }
+        
+    
+    /// TODO kk:20220320 - Use a local type until the decision is made whether or not to use type Substance.
+    type ReactionInfo =
+        {
+            input : list<Species * int>
+            output : list<Species * int>
+        }
+        
+    let normalized i =
+        let normalize d =
+            d
+            |> List.map (fun (s, i) -> [ for _ in 0..(i-1) -> s ])
+            |> List.concat
+            |> List.sort
+
+        {
+            inputNormalized = i.input |> normalize
+            outputNormalized = i.output |> normalize
+        }
+        
+        
     type NoOfMolecules =
         | NoOfMolecules of int
         
@@ -22,22 +76,22 @@ module SsaSolver =
 
     type ReactionRateInfo =
         {
-            reactionInfo : ReactionNormalizedInfo
-            reactionRate : ReactionRate
+            info : ReactionNormalizedInfo
+            rate : ReactionRate
         }
         
         
     let getPropensity i m =
-        match i.reactionInfo.inputNormalized with
+        match i.info.inputNormalized with
         | [] -> 0.0
         | _ ->
-            i.reactionInfo.inputNormalized
+            i.info.inputNormalized
             |> List.map (fun e -> m |> Map.tryFind e |> Option.defaultValue NoOfMolecules.defaultValue)
             |> List.map (fun e -> double e.value)
-            |> List.fold (fun acc r -> acc * r) i.reactionRate.value
+            |> List.fold (fun acc r -> acc * r) i.rate.value
 
     
-    type SubstanceMap = Map<Substance, NoOfMolecules>
+    type SubstanceMap = Map<Species, NoOfMolecules>
         
 
     let evolveSubstances m r =
@@ -79,16 +133,23 @@ module SsaSolver =
                 | h :: t ->
                     let newSum = sum + (getPropensity h s.state)
                     
-                    if newSum >= r2a0 then h.reactionInfo
+                    if newSum >= r2a0 then h.info
                     else inner newSum t 
             
             
             inner 0.0 s.reactions
         
-        member s.evolve r1 r2 =
+        member s.evolve g1 g2 =
+            let r1 = g1() % 1.0
+            let r2 = g2() % 1.0
             let a0 = s.totalPropensity
-            let deltaT = - (log r1) / (a0 / s.volume)
-            let r = s.getReactionRateInfo (r2 * a0)
             
-            { s with time = s.time + deltaT; state = evolveSubstances s.state r } 
+            if a0 > 0.0
+            then
+                let deltaT = - (log r1) / (a0 / s.volume)
+                let r = s.getReactionRateInfo (r2 * a0)
+                
+                { s with time = s.time + deltaT; state = evolveSubstances s.state r }
+            else
+                s
 
--- CoreClm/Samples/GillespieExample/Program.fs
+++ CoreClm/Samples/GillespieExample/Program.fs
@@ -1,2 +1,104 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
+﻿open System
+open Gillespie.SsaSolver
+
 printfn "Hello from F#"
+
+let fox = Any "fox"
+let hare = Any "hare"
+
+//let a = 1.0
+//let b = 1.0
+//let c = 1.0
+//let d = 1.0
+//let noOfFoxes = 20
+//let noOfHares = 20
+
+let a = 0.1
+let b = 0.1
+let c = 0.1
+let d = 0.1
+let noOfFoxes = 2
+let noOfHares = 1000
+
+// https://en.wikipedia.org/wiki/Lotka%E2%80%93Volterra_equations
+let reactions =
+    [
+        // dx / dt = a * x
+        {
+            info = 
+                {
+                    input = [ hare, 1 ]
+                    output = [ hare, 2 ]
+                }
+                |> normalized
+            rate = ReactionRate a
+        }
+        
+        // dx / dt = -b * x * y
+        {
+            info = 
+                {
+                    input = [ (hare, 1); (fox, 1) ]
+                    output = [ (fox, 1) ]
+                }
+                |> normalized
+            rate = ReactionRate b
+        }            
+        
+        // dy / dt = d * x * y
+        {
+            info = 
+                {
+                    input = [ (fox, 1); (hare, 1) ]
+                    output = [ (fox, 2); (hare, 1) ]
+                }
+                |> normalized
+            rate = ReactionRate d
+        }            
+        
+        // dy / dt = -c * x
+        {
+            info = 
+                {
+                    input = [ fox, 1 ]
+                    output = [  ]
+                }
+                |> normalized
+            rate = ReactionRate c
+        }
+    ]
+    
+    
+let v0 =
+    {
+        state =
+            [
+                fox, NoOfMolecules noOfFoxes
+                hare, NoOfMolecules noOfHares
+            ]
+            |> Map.ofList
+        reactions = reactions
+        volume = 1.0
+        time = 0.0
+    }
+    
+let r1 = Random(1)
+let r2 = Random(2)
+
+
+let steps = [ for i in 0..1000 -> i ]
+let evolution =
+    steps
+    
+printfn $"v0: %A{v0}"
+let v1 = v0.evolve r1.NextDouble r2.NextDouble
+printfn $"v1: %A{v1}"
+
+let evolve (v : StateVector) i =
+    let vNew = v.evolve r1.NextDouble r2.NextDouble
+    printfn $"{i}, t: {vNew.time}, population: {vNew.state}"
+    vNew
+
+steps
+|> List.fold (fun acc r -> evolve acc r) v0
+|> ignore
