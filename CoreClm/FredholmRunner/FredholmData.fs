7e15e10bc37051bb7dcd616688e82199b56e051d
kkkmail <kkk@jfcglobal.net>
2023-04-04 20:58:14 -0400

    NET 7.0.3 - Fredholm

--- CoreClm/FredholmRunner/FredholmData.fs
+++ CoreClm/FredholmRunner/FredholmData.fs
@@ -9,42 +9,73 @@ module FredholmData =
         member r.value = let (EnantiomericExcess v) = r in v
 
 
-    /// Data that describe a rectangle in ee * information space.
+    /// Describes a function domain suitable for integral approximation.
+    type Domain =
+        {
+            points : double[]
+            midPoints : double[]
+            intervals : double[]
+        }
+
+        member d.minValue = d.points[0]
+        member d.maxValue = d.points[d.points.Length - 1]
+        member d.range = (d.minValue, d.maxValue)
+        member d.noOfPoints = d.points.Length
+        member d.noOfIntervals = d.intervals.Length
+
+        member d.integrateValues v =
+            d.intervals
+            |> Array.zip v
+            |> Array.map (fun (a, b) -> a * b)
+            |> Array.sum
+
+        member d.integrate f =
+            let values = d.midPoints |> Array.map f
+            d.integrateValues values
+
+        static member create noOfIntervals minValue maxValue =
+            let points = [| for i in 0..noOfIntervals -> minValue + (maxValue - minValue) * (double i) / (double noOfIntervals) |]
+
+            {
+                points = points
+                midPoints = [| for i in 0..noOfIntervals - 1 -> (points[i + 1] + points[i]) / 2.0 |]
+                intervals = [| for i in 0..noOfIntervals - 1 -> (points[i + 1] - points[i]) |]
+            }
+
+
+    /// Data that describes a rectangle in ee * information space.
     /// ee space is naturally limited to [-1, 1] unless we use a conformal transformation to extend it to [-Infinity, Infinity].
     /// inf (information) space is naturally limited at the lower bound (0). The upper bound can be rescaled to any number or even taken to Infinity.
     type DomainData =
         {
-            eeNumberOfPoints : int
-            eePoints : double[]
-            infNumberOfPoints : int
-            infMinValue : double
-            infMaxValue : double
-            infPoints : double[]
+            eeDomain : Domain
+            infDomain : Domain
         }
 
         static member eeMinValue = -1.0
         static member eeMaxValue = 1.0
         static member infDefaultMinValue = 0.0
-        member e.eeRange = (DomainData.eeMinValue, DomainData.eeMaxValue)
-        member e.infRange = (e.infMinValue, e.infMaxValue)
 
         static member create n l2 =
-            let noOfPoints = 2 * n + 1
-            let infMinValue = DomainData.infDefaultMinValue
-            let eePoints = [| for i in 0..noOfPoints -> DomainData.eeMinValue + (DomainData.eeMaxValue - DomainData.eeMinValue) * (double i) / (double noOfPoints) |]
-            let infPoints = [| for i in 0..noOfPoints -> infMinValue + (l2 - infMinValue) * (double i) / (double noOfPoints) |]
+            let noOfIntervals = 2 * n
 
             {
-                eeNumberOfPoints = noOfPoints
-                eePoints = eePoints
-                infNumberOfPoints = noOfPoints
-                infMinValue = infMinValue
-                infMaxValue = l2
-                infPoints = infPoints
+                eeDomain = Domain.create noOfIntervals DomainData.eeMinValue DomainData.eeMinValue
+                infDomain = Domain.create noOfIntervals DomainData.infDefaultMinValue l2
             }
 
 
+    /// Creates a normalized probability
     type MutationProbability =
         {
-            a : int
+            p : double[]
         }
+
+        static member create (d : Domain) m e =
+            let f x = exp (- pown ((x - m) / e) 2)
+            let norm = d.integrate f
+            let p = d.midPoints |> Array.map (fun x -> (f x) / norm)
+
+            {
+                p = p
+            }
