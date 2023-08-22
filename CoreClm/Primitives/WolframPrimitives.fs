16809164bfcddcf10c20dc19b6dd93d071f24225
kkkmail <kkk@jfcglobal.net>
2023-08-22 08:27:11 -0400

    20230822

---
+++ CoreClm/Primitives/WolframPrimitives.fs
@@ -0,0 +1,14 @@
+﻿namespace Primitives
+
+open System
+
+module WolframPrimitives =
+
+    let toMathematicaNotation x =
+        let rec inner x joiner =
+            match box x with
+            | :? float as num -> $"{num:E}".Replace("E", "*^")
+            | :? seq<_> as seq -> (seq |> Seq.fold (fun acc item -> acc + joiner + (inner item ",\n ")) "{") + "}"
+            | _ -> $"{x}"
+        inner x ", "
+
