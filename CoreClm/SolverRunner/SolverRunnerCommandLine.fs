a94fb464885d07fa5ef646a5e83df2faf7c5ba6f
kkkmail <kkk@jfcglobal.net>
2024-09-09 08:00:35 -0400

    Update SolverRunnerCommandLine.fs

--- CoreClm/SolverRunner/SolverRunnerCommandLine.fs
+++ CoreClm/SolverRunner/SolverRunnerCommandLine.fs
@@ -5,15 +5,16 @@ open System
 
 
 module SolverRunnerCommandLine =
+    let x = 1
 
-    [<CliPrefix(CliPrefix.None)>]
-    type SolverRunnerArguments =
-        | [<Mandatory>] [<Unique>] [<AltCommandLine("q")>] RunQueue of Guid
-        | [<Unique>] [<AltCommandLine("f")>] ForceRun of bool
+    //[<CliPrefix(CliPrefix.None)>]
+    //type SolverRunnerArguments =
+    //    | [<Mandatory>] [<Unique>] [<AltCommandLine("q")>] RunQueue of Guid
+    //    | [<Unique>] [<AltCommandLine("f")>] ForceRun of bool
 
-    with
-        interface IArgParserTemplate with
-            member s.Usage =
-                match s with
-                | RunQueue _ -> "specify RunQueueId."
-                | ForceRun _ -> "specify true to force running without processor count limitation."
+    //with
+    //    interface IArgParserTemplate with
+    //        member s.Usage =
+    //            match s with
+    //            | RunQueue _ -> "specify RunQueueId."
+    //            | ForceRun _ -> "specify true to force running without processor count limitation."
